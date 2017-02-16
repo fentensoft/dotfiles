24cada22752849615cfa81c09ab3585949eaf069
Tony Liu <388848@163.com>
2017-02-16 19:46:37 +0800

    Use vim-plug as Vim plugin manager.
    Signed-off-by: Tony Liu <388848@163.com>

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -sf $(pwd)/.zshrc ~/.zshrc
 
 #Install Vim
 sudo apt install vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim +PluginInstall +qall
 ln -sf $(pwd)/.vimrc ~/.vimrc
